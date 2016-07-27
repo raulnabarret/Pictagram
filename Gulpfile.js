b9c066b1aefdd01ce07cf4a1c52bc1eba2b076fc
raulnabarret <raulnabarret@gmail.com>
2016-07-27 18:36:32 -0500

    Automatizar el proceso de build

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,31 +4,51 @@ var rename = require('gulp-rename')
 var babel = require('babelify')
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
+var watchify = require('watchify')
 
 gulp.task('styles', function() {
-    
-	gulp.src('index.scss')			//Source
-	  .pipe(sass())					//Función o método: Sass
-	  .pipe(rename('app.css'))		//Función o método: Rename
-	  .pipe(gulp.dest('public')) 	//Destination
+
+    gulp.src('index.scss') //Source
+        .pipe(sass()) //Función o método: Sass
+        .pipe(rename('app.css')) //Función o método: Rename
+        .pipe(gulp.dest('public')) //Destination
 
 })
 
 gulp.task('assets', function() {
 
-	gulp.src('assets/*')
-	  .pipe(gulp.dest('public'))
+    gulp.src('assets/*')
+        .pipe(gulp.dest('public'))
 })
 
-gulp.task('scripts', function() {
+function compile(watch) {
+    var bundle = watchify(browserify('./src/index.js'))
 
-	browserify('./src/index.js')
-		.transform(babel)
-		.bundle()
-		.pipe(source('index.js'))
-		.pipe(rename('app.js'))
-		.pipe(gulp.dest('public'))
+    function rebundle() {
+    	bundle
+        	.transform(babel)
+            .bundle()
+            .pipe(source('index.js'))
+            .pipe(rename('app.js'))
+            .pipe(gulp.dest('public'))
+    }
 
+    if (watch) {
+        bundle.on('update', function() {
+            console.log('--> Bundling ...')
+            rebundle()
+        })
+    }
+
+    rebundle()
+}
+
+gulp.task('build', function() {
+	return compile()
 })
 
-gulp.task('default', ['styles', 'assets', 'scripts'])
+gulp.task('watch', function() {
+    return compile(true)
+})
+
+gulp.task('default', ['styles', 'assets', 'build'])
