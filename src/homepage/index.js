3945021acec745014a36772832e20ba548b45f79
raulnabarret <raulnabarret@gmail.com>
2016-08-02 18:26:50 -0500

    Requests al servidor usando promises con Axios

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -4,10 +4,11 @@ var title = require('title')
 var template = require('./template')
 var request = require('superagent')
 var header = require('../header')
+var axios = require('axios')
 
 var $main = $('#main-container')
 
-page('/', header, loadPictures, function(ctx, next) {
+page('/', header, loadPicturesAxios, function(ctx, next) {
 	
 	title('Pictagram - Home')
 
@@ -16,13 +17,26 @@ page('/', header, loadPictures, function(ctx, next) {
 
 })
 
-function loadPictures(ctx, next) {
-	request
+function loadPicturesAxios(ctx, next) {
+	axios
 		.get('/api/pictures')
-		.end(function (err, res){
-			if (err) return console.log(err)
+		.then(function (res){
 
-			ctx.pictures = res.body
+			ctx.pictures = res.data
 			next()
 		})
+		.catch(function (err) {
+			console.log(err)
+		})
 }	
+
+// function loadPictures(ctx, next) {
+// 	request
+// 		.get('/api/pictures')
+// 		.end(function (err, res){
+// 			if (err) return console.log(err)
+
+// 			ctx.pictures = res.body
+// 			next()
+// 		})
+// }	
