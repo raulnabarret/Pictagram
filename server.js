b00ad6ca7d400003b2672334dbed1055960f9b95
raulnabarret <raulnabarret@gmail.com>
2016-08-02 18:05:42 -0500

    Requests al servidor usando Superagent

--- server.js
+++ server.js
@@ -23,6 +23,37 @@ app.get('/signup', function (req, res) {
 	})
 })
 
+app.get('/api/pictures', function(req, res) {
+
+	var pictures = [
+			{
+			'user': {
+				username: 'raulnabarret',
+				avatar: 'https://pbs.twimg.com/profile_images/755066211621179392/IWpe9VDE.jpg'
+			},
+			'url': 'https://pbs.twimg.com/profile_images/755066211621179392/IWpe9VDE.jpg',
+			'likes': 0,
+			'liked': false,
+			'createdAt': new Date().getTime()
+		},
+		{
+			'user': {
+				username: 'raulnabarret',
+				avatar: 'https://pbs.twimg.com/profile_images/755066211621179392/IWpe9VDE.jpg'
+			},
+			'url': 'https://pbs.twimg.com/profile_images/755066211621179392/IWpe9VDE.jpg',
+			'likes': 2,
+			'liked': true,
+			'createdAt': new Date().getTime()
+		}
+	]
+
+	setTimeout(function() {
+		res.send(pictures)
+	}, 2000);
+
+})
+
 app.listen(8080, function (err) {
 	if (err) return console.log('Error'), process.exit(1)
 
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,39 +2,27 @@ var $ = require('jquery')
 var page = require('page')
 var title = require('title')
 var template = require('./template')
-
+var request = require('superagent')
+var header = require('../header')
 
 var $main = $('#main-container')
 
-page('/', function(ctx, next) {
+page('/', header, loadPictures, function(ctx, next) {
 	
 	title('Pictagram - Home')
-    
-	var pictures = [
-		{
-			'user': {
-				username: 'raulnabarret',
-				avatar: 'https://pbs.twimg.com/profile_images/755066211621179392/IWpe9VDE.jpg'
-			},
-			'url': 'https://pbs.twimg.com/profile_images/755066211621179392/IWpe9VDE.jpg',
-			'likes': 0,
-			'liked': false,
-			'createdAt': new Date()
-		},
-		{
-			'user': {
-				username: 'raulnabarret',
-				avatar: 'https://pbs.twimg.com/profile_images/755066211621179392/IWpe9VDE.jpg'
-			},
-			'url': 'https://pbs.twimg.com/profile_images/755066211621179392/IWpe9VDE.jpg',
-			'likes': 2,
-			'liked': true,
-			'createdAt': new Date().setDate(new Date().getDate() - 10)
-		}
-	]
 
-    $main.html(template(pictures))
+    $main.html(template(ctx.pictures))
 
 
 })
-	
+
+function loadPictures(ctx, next) {
+	request
+		.get('/api/pictures')
+		.end(function (err, res){
+			if (err) return console.log(err)
+
+			ctx.pictures = res.body
+			next()
+		})
+}	
